d28925714525a53e639e9274e9a032fb341e7b57
ashiqurras <ashiqurrussel2@gmail.com>
2024-10-29 20:07:54 +0100

    Add header for project and pass props

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -8,27 +8,16 @@ type Props = {
 };
 
 const Project = ({ params }: Props) => {
-  const [projectId, setProjectId] = useState<string | null>(null);
+  const { id } = params;
+
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
-  useEffect(() => {
-    async function fetchParams() {
-      const unwrappedParams = await params;
-      setProjectId(unwrappedParams.id);
-    }
-    fetchParams();
-  }, [params]);
-
-  if (!projectId) {
-    return <div>Loading...</div>;
-
-    return (
-      <div>
-        <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-        {/* Add other JSX elements */}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {/* Add other JSX elements */}
+    </div>
+  );
 };
 export default Project;
