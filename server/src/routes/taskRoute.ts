f2a252f742d9dae42e5d2e6465060c5c9306a310
ashiqurras <ashiqurrussel2@gmail.com>
2024-10-29 01:47:45 +0100

    Implement update task status api service

--- server/src/routes/taskRoute.ts
+++ server/src/routes/taskRoute.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getTasks, createTask } from "../controllers/taskController";
+import {
+  getTasks,
+  createTask,
+  updateTaskStatus,
+} from "../controllers/taskController";
 
 const router = Router();
 
 router.get("/", getTasks);
 router.post("/create", createTask);
+router.patch("/:taskId/status", updateTaskStatus);
 
 export default router;
