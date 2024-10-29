428ea1683af705d8904c577ef7c72b0a769b4b51
ashiqurras <ashiqurrussel2@gmail.com>
2024-10-29 21:53:22 +0100

    Add Tab list on each product click and fix param warning

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -2,22 +2,85 @@
 
 import React, { useState, useEffect } from "react";
 import ProjectHeader from "../ProjectHeader";
+import { Grid3X3, List, Clock, Table } from "lucide-react";
 
 type Props = {
   params: { id: string };
 };
 
 const Project = ({ params }: Props) => {
-  const { id } = params;
-
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
+  const [projectId, setProjectId] = useState("");
+
+  useEffect(() => {
+    const getId = async () => {
+      const paramId = await params.id;
+      setProjectId(paramId);
+    };
+  }, [params]);
+
   return (
-    <div>
-      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-      {/* Add other JSX elements */}
+    <div className="px-4 xl:px-6">
+      <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
+        <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+        {/* Add other JSX elements */}
+      </div>
+
+      <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center">
+        <div className="flex flex-1 items-center gap-2 md:gap-4">
+          <TabButton
+            name="Board"
+            icon={<Grid3X3 className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="List"
+            icon={<List className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Timeline"
+            icon={<Clock className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Table"
+            icon={<Table className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+        </div>
+      </div>
     </div>
   );
 };
+
+type TabButtonProps = {
+  name: string;
+  icon: React.ReactNode;
+  setActiveTab: (tabName: string) => void;
+  activeTab: string;
+};
+
+const TabButton = ({ name, icon, setActiveTab, activeTab }: TabButtonProps) => {
+  const isActive = activeTab === name;
+
+  return (
+    <button
+      className={`relative flex items-center gap-2 px-1 py-2 text-gray-500 after:absolute after:-bottom-[9px] after:left-0 after:h-[1px] after:w-full hover:text-blue-600 dark:text-neutral-500 dark:hover:text-white sm:px-2 lg:px-4 ${
+        isActive ? "text-blue-600 after:bg-blue-600 dark:text-white" : ""
+      }`}
+      onClick={() => setActiveTab(name)}
+    >
+      {icon}
+      {name}
+    </button>
+  );
+};
+
 export default Project;
