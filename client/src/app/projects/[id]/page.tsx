4dfe41d9d85a65457795c89c5cde54ac4ff87810
ashiqurras <ashiqurrussel2@gmail.com>
2024-11-23 14:51:20 +0100

    Fix params unresolved issue

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,35 +1,21 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ProjectHeader from "../ProjectHeader";
-import { Grid3X3, List, Clock, Table } from "lucide-react";
+import { useParams } from "next/navigation";
 
-type Props = {
-  params: { id: string } | Promise<{ id: string }>;
-};
-
-const Project = ({ params }: Props) => {
+const Project = () => {
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
-  const [projectId, setProjectId] = useState("");
 
-  useEffect(() => {
-    const getId = async () => {
-      const resolvedParams = await params;
-      setProjectId(resolvedParams.id);
-    };
-
-    if (params instanceof Promise) {
-      getId();
-    } else {
-      setProjectId(params.id);
-    }
-  }, [params]);
+  const params = useParams();
+  const id = params?.id;
 
   return (
     <div className="px-4 xl:px-6">
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
         <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+        {/* Add other JSX elements */}
       </div>
     </div>
   );
