388016772dda4ca33f6eede0b5da754e8441367b
ashiqurras <ashiqurrussel2@gmail.com>
2024-11-23 14:38:27 +0100

    resolve promise

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -5,27 +5,31 @@ import ProjectHeader from "../ProjectHeader";
 import { Grid3X3, List, Clock, Table } from "lucide-react";
 
 type Props = {
-  params: { id: string };
+  params: { id: string } | Promise<{ id: string }>;
 };
 
 const Project = ({ params }: Props) => {
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
-
   const [projectId, setProjectId] = useState("");
 
   useEffect(() => {
     const getId = async () => {
-      const paramId = await params.id;
-      setProjectId(paramId);
+      const resolvedParams = await params;
+      setProjectId(resolvedParams.id);
     };
+
+    if (params instanceof Promise) {
+      getId();
+    } else {
+      setProjectId(params.id);
+    }
   }, [params]);
 
   return (
     <div className="px-4 xl:px-6">
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
         <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-        {/* Add other JSX elements */}
       </div>
     </div>
   );
--- server/dist/index.js
+++ server/dist/index.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 });
 app.use("/projects", projectsRoute_1.default);
 app.use("/tasks", taskRoute_1.default);
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 5000;
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on part ${port}`);
 });
