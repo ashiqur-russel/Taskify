34450dc3ded4e33bb271c685e3a146d235e2077c
ashiqur-russel <ashiqurrussel2@gmail.com>
2025-06-08 22:43:23 +0200

    created search controller, route

--- server/dist/index.js
+++ server/dist/index.js
@@ -11,6 +11,7 @@ const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const projectsRoute_1 = __importDefault(require("./routes/projectsRoute"));
 const taskRoute_1 = __importDefault(require("./routes/taskRoute"));
+const searchRoute_1 = __importDefault(require("./routes/searchRoute"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 });
 app.use("/projects", projectsRoute_1.default);
 app.use("/tasks", taskRoute_1.default);
+app.use("/search", searchRoute_1.default);
 const port = Number(process.env.PORT) || 5001;
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on part ${port}`);
